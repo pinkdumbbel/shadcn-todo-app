44ccc413afc614f93c9f3f047f1b8c5661ab8103
pinkdumbbel <tlacjd665@gmail.com>
2025-01-18 16:33:07 +0900

    chore: tailwindcss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css'
-
 function App() {
-
   return (
-    <div>
-      이곳에 To-Do App을 작성해주세요.
+    <div className="bg-black text-red-800">
+      이곳에 To-Do App을 작성해주세요.222
     </div>
   )
 }
