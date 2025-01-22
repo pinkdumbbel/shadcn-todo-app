8308ca2f0350016408b4d093d8bea36a16a1e913
pinkdumbbel <tlacjd665@gmail.com>
2025-01-22 17:45:29 +0900

    fix: TodoList에 스크롤 적용

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 
 const App = () => (
   <main className="w-screen h-screen flex justify-center items-center">
-    <article className="w-[50%] max-h-[80%] flex flex-col p-[40px] border-[1px] border-solid border-gray-300 rounded-xl shadow-xl">
+    <article className="w-[60%] flex flex-col p-[40px] border-[1px] border-solid border-gray-300 rounded-xl shadow-xl">
       <TodoProvider>
         <TodoSearchFilterProvider>
           <TodoPaginationProvider>
--- src/components/todo/list/TodoList.tsx
+++ src/components/todo/list/TodoList.tsx
@@ -4,6 +4,7 @@ import * as Table from '@/components/ui/table'
 import { useFilterTodo } from '@/hooks'
 import { useTodoRowCheckActionContext, useTodoRowCheckContext } from '@/context'
 import { DeadlineCell, TodoTextCell, DoneCell, ActionCell } from './cells'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 export const TodoList = () => {
   const { todos } = useFilterTodo()
@@ -12,44 +13,50 @@ export const TodoList = () => {
 
   return (
     <section className="mt-[20px] border-[1px] border-solid border-gray-300 rounded-lg">
-      <Table.Table>
-        <Table.TableHeader>
-          <Table.TableRow className="hover:bg-transparent">
-            <Table.TableHead className="w-[24px]">
-              <Checkbox
-                className="w-[24px] h-[24px]"
-                checked={todoIds.length > 0 && todos.length === todoIds.length}
-                onClick={checkAllTodo}
-              />
-            </Table.TableHead>
-            <Table.TableHead className="text-[20px]">Todo</Table.TableHead>
-            <Table.TableHead className="text-[20px]">Deadline</Table.TableHead>
-            <Table.TableHead className="text-[20px]">Done</Table.TableHead>
-          </Table.TableRow>
-        </Table.TableHeader>
-
-        <Table.TableBody>
-          {todos.map((todo) => (
-            <Table.TableRow key={todo.id}>
-              <Table.TableCell>
+      <ScrollArea className="h-[860px]" type="always">
+        <Table.Table>
+          <Table.TableHeader className="h-[60px]">
+            <Table.TableRow className="hover:bg-transparent">
+              <Table.TableHead className="w-[24px]">
                 <Checkbox
                   className="w-[24px] h-[24px]"
-                  checked={todoIds.includes(todo.id)}
-                  onClick={() => checkTodo(todo.id)}
+                  checked={
+                    todoIds.length > 0 && todos.length === todoIds.length
+                  }
+                  onClick={checkAllTodo}
                 />
-              </Table.TableCell>
-
-              <TodoTextCell done={todo.done} text={todo.text} />
-
-              <DeadlineCell deadline={todo.deadline} />
-
-              <DoneCell done={todo.done} />
-
-              <ActionCell id={todo.id} />
+              </Table.TableHead>
+              <Table.TableHead className="text-[20px]">Todo</Table.TableHead>
+              <Table.TableHead className="text-[20px]">
+                Deadline
+              </Table.TableHead>
+              <Table.TableHead className="text-[20px]">Done</Table.TableHead>
             </Table.TableRow>
-          ))}
-        </Table.TableBody>
-      </Table.Table>
+          </Table.TableHeader>
+
+          <Table.TableBody>
+            {todos.map((todo) => (
+              <Table.TableRow key={todo.id} className="h-[80px]">
+                <Table.TableCell>
+                  <Checkbox
+                    className="w-[24px] h-[24px]"
+                    checked={todoIds.includes(todo.id)}
+                    onClick={() => checkTodo(todo.id)}
+                  />
+                </Table.TableCell>
+
+                <TodoTextCell done={todo.done} text={todo.text} />
+
+                <DeadlineCell deadline={todo.deadline} />
+
+                <DoneCell done={todo.done} />
+
+                <ActionCell id={todo.id} />
+              </Table.TableRow>
+            ))}
+          </Table.TableBody>
+        </Table.Table>
+      </ScrollArea>
     </section>
   )
 }
